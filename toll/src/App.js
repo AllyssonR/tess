1865568df73fbe865832e9780a3b58db4d00275a
AllyssonR <AllyssonR@users.noreply.github.com>
2020-05-10 15:40:20 -0300

    atualizando cod

--- toll/src/App.js
+++ toll/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import Primeiro from './Components/Primeiro';
+import ComParametro from './Components/ComParametro';
+import './App.css';
 const App  = ()=> (
+  <React.StrictMode>
   <Primeiro/>
+  <ComParametro titulo="Esse é o titulo" subtitulo="Esse é o subtitulo"/>
+  </React.StrictMode>
 );
 
 export default App;
---
+++ toll/src/Components/ComParametro.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default props =>(
+<>
+
+<h2>{props.titulo}</h2>
+<p>{props.subtitulo}</p>
+
+</>
+)
+
